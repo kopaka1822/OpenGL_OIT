4e3d9b3c95ec2b8f994718470098a1e10af09231
Felix Brüll <felixbruell@live.de>
2018-02-27 21:30:37 +0100

    fixed update issue

--- ForwardRenderer/Dependencies/gl/buffer.hpp
+++ ForwardRenderer/Dependencies/gl/buffer.hpp
@@ -77,12 +77,6 @@ namespace gl
 		}
 
 		template<class T, bool TEnabled = (TUsage & GL_DYNAMIC_STORAGE_BIT) != 0>
-		std::enable_if_t<TEnabled> update(const T& data)
-		{
-			update(&data, 0, sizeof T);
-		}
-
-		template<class T, bool TEnabled = (TUsage & GL_DYNAMIC_STORAGE_BIT) != 0>
 		std::enable_if_t<TEnabled> update(const std::vector<T>& data)
 		{
 			update(data.data(), 0, data.size() * sizeof T);
