572da4abd8aaf5631abc6cbb8a6342d8fcc45ccf
Felix Brüll <felixbruell@live.de>
2018-03-22 21:47:46 +0100

    added #include compability to shader loading (without hot reload for now)

--- ForwardRenderer/Graphics/HotReloadShader.cpp
+++ ForwardRenderer/Graphics/HotReloadShader.cpp
@@ -6,6 +6,9 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
+#include <filesystem>
+namespace fs = std::experimental::filesystem;
+
 #ifndef WIN32
 #include <unistd.h>
 #define stat _stat
@@ -129,6 +132,60 @@ std::shared_ptr<HotReloadShader::WatchedProgram> HotReloadShader::loadProgram(
 	return newProgram;
 }
 
+std::string loadShaderSource(const fs::path& filename)
+{
+	std::ifstream file;
+	file.open(filename);
+
+	std::string output;
+	std::string line;
+
+	if (!file.is_open())
+		throw std::runtime_error("could not open file " + filename.string());
+
+	size_t lineNumber = 0;
+	while(file.good())
+	{
+		std::getline(file, line);
+		++lineNumber;
+
+		// check for #include 
+		// check line for #include
+
+		const auto includeStart = line.find("#include");
+		auto commentStart = std::string::npos;
+		if (includeStart != std::string::npos) // commented out?
+			commentStart = line.find("//");
+
+		if (includeStart != std::string::npos && includeStart < commentStart)
+		{
+			// we have an include
+			const auto parStart = line.find('\"', includeStart + strlen("#include"));
+			if (parStart == std::string::npos)
+				throw std::runtime_error("missing 1. \" for include");
+
+			const auto parEnd = line.find('\"', parStart + 1);
+			if (parEnd == std::string::npos)
+				throw std::runtime_error("missing 2. \" for include");
+
+			const auto includeFile = line.substr(parStart + 1, parEnd - parStart - 1);
+			// put path together
+			const auto includePath = filename.parent_path().append(includeFile);
+
+			// parse this file
+			output += "#line 1\n";
+			output += loadShaderSource(includePath);
+			output += "\n#line " + std::to_string(lineNumber + 1) + '\n';
+		}
+		else
+		{
+			output.append(line + '\n');
+		}
+	}
+
+	return output;
+}
+
 void HotReloadShader::loadShader(WatchedShader& dest)
 {
 	// create new shader object
@@ -139,22 +196,7 @@ void HotReloadShader::loadShader(WatchedShader& dest)
 	// update last modified
 	dest.m_lastModified = getLastModified(filename);
 
-	// load file
-	std::ifstream file;
-	file.open(filename);
-
-	std::string output;
-	std::string line;
-	if (!file.is_open())
-		throw std::runtime_error("could not open file " + filename);
-
-
-	while (file.good())
-	{
-		std::getline(file, line);
-		// TODO add inlcude
-		output.append(line + "\n");
-	}
+	auto output = loadShaderSource(filename);
 
 	// compile shader
 	const char* source = output.c_str();
