808d93c9ef160e3668c8fc4a90a4c9fe54e23237
Felix <felixbruell@live.de>
2018-05-17 11:43:04 +0200

    adjustment in aoit unsorted heights

--- ForwardRenderer/Shader/AdaptiveBuildVisibility.fs
+++ ForwardRenderer/Shader/AdaptiveBuildVisibility.fs
@@ -75,7 +75,8 @@ void insertAlpha(float one_minus_alpha, float depth)
 	else
 	{
 		// avoid dividing by zero
-		g_visExponent = log(productAlpha / max(lastAlpha, 0.00000001)) / max(maxDepth - minDepth, 0.00000001);
+		//g_visExponent = log(productAlpha / max(lastAlpha, 0.00000001)) / max(maxDepth - minDepth, 0.00000001);
+		g_visExponent = log(productAlpha) / max(maxDepth - minDepth, 0.00000001);
 		g_visOffset = -minDepth;
 
 		float minHeight = FLOAT_MAX;
